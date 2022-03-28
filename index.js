95fd361b46524e3e5a7eee461de651aea27573ad
Devin Jaggernauth <neexis@gmail.com>
2022-03-28 17:28:11 -0400

    GET test

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql');
-
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'spiral0ut',
-  database: 'TestDb',
-});
+const mysql = require('mysql2');
+const { response } = require('express');
 
 const app = express();
 app.use(cors());
-const {PORT = 3000} = process.env;
+app.use(express.json())
+const {PORT = 3000, DB_HOST, DB_PASSWORD, DB_NAME} = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Now serving');
+const db = mysql.createPool({
+  host:  DB_HOST,
+  user: process.env.DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
+
+// get all stats in database, locations, quantities locally and globally
+// GET 
+// select product based on name OR id
+// view locations and update quantities
+
+app.get('/:coreId', (req, res) => {
+  const query = `SELECT Core_Number, Internal_Title, Location, Quantity FROM cores JOIN locations ON cores.Core_Number = locations.Product_Code WHERE Core_Number LIKE ('%${req.params.coreId}') OR Internal_Title LIKE('${req.params.coreId}%')`
+  db.query(query, (error, results) =>{
+    if(!results[0]){
+      res.status(404).send({Error: `Product not found. Check product ID or name and try again.`})
+      return
+    }
+    res.send(results )
+  })
+});
+
+app.get('/prod/:name', (req, res) => {
+  
+})
+
+app.put('/prod/:quantity', (req, res)=>{
+  
+})
 
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
