46d582cca893e3f7e7f3f783c39847f22eb598ef
Devin Jaggernauth <neexis@gmail.com>
2022-03-29 08:48:23 -0400

    retry DB

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.options('*', cors());
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 const PORT = process.env.PORT || 4000;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host:  DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
