9654d86bbe88e79d7a8690f5abdf4001d99748fc
Devin Jaggernauth <neexis@gmail.com>
2022-03-29 11:16:54 -0400

    mysql

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
-const database = mysql.createPool({
+const database = mysql.createConnection({
   host:  DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
