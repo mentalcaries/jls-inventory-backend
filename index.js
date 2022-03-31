46ede45f803de9d380f09b5e9b48dc5e6cee89c3
Devin Jaggernauth <neexis@gmail.com>
2022-03-30 21:11:32 -0400

    update cors

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(cors());
 app.use(express.json())
 app.options('*', cors());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
 
 const PORT = process.env.PORT || 4000;
 
